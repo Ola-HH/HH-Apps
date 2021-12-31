27a84abf1d6c5e928bdf868b0a7740f0e2675b14
Ola-HH <96570269+Ola-HH@users.noreply.github.com>
2021-12-31 15:37:19 +0100

    new homepage

--- src/home.jsx
+++ src/home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/Button';
+import HHButton from "./HH-Button";
 
 const Home = () => {
   return (
@@ -9,16 +10,13 @@ const Home = () => {
       <p className="site-title">Velkommen til HH Apps</p>
       <p className="second-title">Velg et spill!</p>
       <div>
-        <ButtonGroup orientation="vertical" aria-label="vertical outlined button group">
+        <ButtonGroup orientation="vertical" aria-label="vertical outlined button group" className="meny">
           <NavLink to="/liars-dice">
-            <Button>         
-              Liars dice
-            </Button>
+            <HHButton text="Liars dice" />         
           </NavLink>
+          <br></br>
           <NavLink to="/gruble">
-            <Button>
-              Gruble
-            </Button>
+            <HHButton text="Gruble" />  
           </NavLink> 
         </ButtonGroup>
       </div>
