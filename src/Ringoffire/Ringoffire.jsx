9f6101cc19d15b276eb4bc6c04e585da451ce895
Ola-HH <96570269+Ola-HH@users.noreply.github.com>
2022-01-18 17:41:29 +0100

    prevented same card showing multiple times in ROF

--- src/Ringoffire/Ringoffire.jsx
+++ src/Ringoffire/Ringoffire.jsx
@@ -257,43 +257,74 @@ function Ringoffire() {
     }
   ]
 
+  const [usedCards, setUsedCards] = useState([]);
   const [card, setCard] = useState(-1);
+  const [done, setDone] = useState(false)
   const newCard = () => {
-    setCard(Math.floor(Math.random()* cards.length))
+    if (usedCards.length === cards.length) {
+      setDone(true);
+      return;
+    }
+    for (let i = 0; i < 2; i++) {
+      const random = Math.floor(Math.random()* cards.length)
+      const temporaryUsed = [];
+      temporaryUsed.concat(usedCards);
+      if (temporaryUsed.includes(random)) {
+        i--;
+        continue
+      }
+      setCard(random);
+      setUsedCards(usedCards.concat(random));
+    }
+  }
+  const restart = () => {
+    setUsedCards([])
+    setDone(false)
+    setCard(-1);
   }
   
 
   return (
     <Stack textAlign="center" spacing={2}>
       <Typography variant="h3" color="primary">Ring of fire</Typography>
-      <HHButton click={newCard} text="Nytt kort" />
-      { card > -1 ? (
-        <Stack textAlign="center" direction="row" justifyContent="center" 
-        sx={{ flexWrap: "wrap" }}>
-          <Stack mb={2} border="1px solid #2b88d8" spacing={1} borderRadius={1} p={1} sx={{
-            width: {
-              sm: "40%",
-              xs: "100%"
-            },
-            mr: {
-              sm: 5,
-              xs: 0
-            }
-          }}>
-            <Typography variant="h4" color="primary">{cards[card].title}</Typography>
-            <Divider />
-            <Typography variant="body1" color="primary">{cards[card].desc}</Typography>
+      
+      { done ? (
+        <>
+          <HHButton click={restart} text="Start en ny runde" />
+          <Typography variant="h5" color="primary">Dere har nå vært igjennom samtlige 52 kort!</Typography>
+        </>
+      ):(
+        <HHButton click={newCard} text="Nytt kort" />
+      )}
+
+      { card > -1 && !done ? (
+          <Stack textAlign="center" direction="row" justifyContent="center" 
+          sx={{ flexWrap: "wrap" }}>
+            <Stack mb={2} border="1px solid #2b88d8" spacing={1} borderRadius={1} p={1} sx={{
+              width: {
+                sm: "40%",
+                xs: "100%"
+              },
+              mr: {
+                sm: 5,
+                xs: 0
+              }
+            }}>
+              <Typography variant="h4" color="primary">{cards[card].title}</Typography>
+              <Divider />
+              <Typography variant="body1" color="primary">{cards[card].desc}</Typography>
+            </Stack>
+            <Box sx={{ justifyContent: "center", textAlign: "center",
+              width: {
+                sm: "40%",
+                xs: "100%"
+              }
+            }}>
+            <img src={require(`./card-imgs/${cards[card].src}`)} alt={cards[card].title} width="100%"/>
+            </Box>
           </Stack>
-          <Box sx={{ justifyContent: "center", textAlign: "center",
-            width: {
-              sm: "40%",
-              xs: "100%"
-            }
-          }}>
-          <img src={require(`./card-imgs/${cards[card].src}`)} alt={cards[card].title} width="100%"/>
-          </Box>
-        </Stack>
-      ):("")}
+        ):("")}
+      
     </Stack>
   );
 }
