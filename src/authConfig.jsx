b609be3c8a2bc64e12d3efb8b1f851f16b96010d
Ola-HH <96570269+Ola-HH@users.noreply.github.com>
2022-01-26 21:35:53 +0100

    some fix

--- src/authConfig.jsx
+++ src/authConfig.jsx
@@ -1,21 +1,19 @@
 export const msalConfig = {
     auth: {
       clientId: process.env.REACT_APP_CLIENT_ID,
-      authority: "https://login.microsoftonline.com/common", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
+      authority: "https://login.microsoftonline.com/common", 
       redirectUri: "https://hagerupsen-hansen.no",
     },
     cache: {
-      cacheLocation: "sessionStorage", // This configures where your cache will be stored
-      storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+      cacheLocation: "sessionStorage", 
+      storeAuthStateInCookie: false, 
     }
   };
   
-  // Add scopes here for ID token to be used at Microsoft identity platform endpoints.
   export const loginRequest = {
    scopes: ["User.Read"]
   };
   
-  // Add the endpoints here for Microsoft Graph API services you'd like to use.
   export const graphConfig = {
       graphMeEndpoint: "https://graph.microsoft.com/v1.0/me"
   };
